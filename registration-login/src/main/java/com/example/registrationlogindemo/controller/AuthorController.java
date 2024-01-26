fad67f3202b8d304b84054aab48ff116dc244195
hellowgh <hellowgh@qq.com>
2024-01-27 05:31:43 +0800

    更换maven依赖

--- registration-login/src/main/java/com/example/registrationlogindemo/controller/AuthorController.java
+++ registration-login/src/main/java/com/example/registrationlogindemo/controller/AuthorController.java
@@ -3,6 +3,7 @@ package com.example.registrationlogindemo.controller;
 import com.example.registrationlogindemo.dto.UserDto;
 import com.example.registrationlogindemo.entity.User;
 import com.example.registrationlogindemo.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Controller
