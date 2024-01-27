36b4827cdf516a7d8c1f4a7408ddbad139590e76
hellowgh <hellowgh@qq.com>
2024-01-27 21:43:41 +0800

    finish

--- registration-login/README.md
+++ registration-login/README.md
@@ -1,6 +1,7 @@
 # Spring Boot User Registration and Login Example Tutorial
 
 [参考资料](https://www.javaguides.net/2018/10/user-registration-module-using-springboot-springmvc-springsecurity-hibernate5-thymeleaf-mysql.html)
+[代码仓](https://github.com/RameshMF/registration-login-springboot-security-thymeleaf/blob/master/src/main/java/com/example/registrationlogindemo/service/impl/UserServiceImpl.java)
 
 ## 技术栈
 
@@ -14,4 +15,5 @@
 
 We validate the user registration fields with Java bean validation annotations with Hibernate validator implementation.
 
-Basically, we will develop a simple User Registration Module using Role-based Spring Security which can use in any spring MVC-based project.
+Basically, we will develop a simple User Registration Module using Role-based Spring Security which can use in any
+spring MVC-based project.
--- registration-login/src/main/java/com/example/registrationlogindemo/entity/User.java
+++ registration-login/src/main/java/com/example/registrationlogindemo/entity/User.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="users")
+@Table(name = "users")
 public class User {
     private static final long serialVersionUID = 1L;
 
@@ -35,7 +34,7 @@ public class User {
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(
             name = "users_roles",
-            joinColumns = {@JoinColumn(name="USER_ID", referencedColumnName = "ID")},
+            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
             inverseJoinColumns = {@JoinColumn(name = "ROLE_ID", referencedColumnName = "ID")}
     )
     private List<Role> roles = new ArrayList<>();
