5b73d3055d1f2fa3690afe1834f9666413a03760
hellowgh <hellowgh@qq.com>
2024-02-08 06:38:41 +0800

    order 服务拉取user服务

--- cloud-demo/order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -1,12 +1,14 @@
 package cn.itcast.order.web;
 
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import cn.itcast.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("order")
@@ -15,9 +17,22 @@ public class OrderController {
    @Autowired
    private OrderService orderService;
 
+   @Autowired
+   private RestTemplate restTemplate;
+
     @GetMapping("{orderId}")
     public Order queryOrderById(@PathVariable("orderId") Long orderId) {
-        // 根据id查询订单并返回
-        return orderService.queryOrderById(orderId);
+        // 1. 查询订单
+        Order order = orderService.queryOrderById(orderId);
+
+        // 2. 根据userId查询user信息
+        String url = "http://userservice/user/" + order.getUserId();
+        User user = restTemplate.getForObject(url, User.class);
+
+        // 3. 组装数据
+        order.setUser(user);
+
+        // 4. 返回数据
+        return order;
     }
 }
