c3ac4b448367a9a3b5c71a36e5fcf151dc34fe4a
hellowgh <hellowgh@qq.com>
2024-02-09 17:47:25 +0800

    给gateway添加filter

--- cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/itcast/user/web/UserController.java
@@ -5,10 +5,7 @@ import cn.itcast.user.pojo.User;
 import cn.itcast.user.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -42,7 +39,9 @@ public class UserController {
      * @return 用户
      */
     @GetMapping("/{id}")
-    public User queryById(@PathVariable("id") Long id) {
+    public User queryById(@PathVariable("id") Long id,
+                          @RequestHeader(value = "X-Request-red", required = false) String color) {
+        System.out.println(color);
         return userService.queryById(id);
     }
 }
